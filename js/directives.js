05ff1706d90c39a24c68d17c0ace2ce725dd9f67
michalkowalczuk <michael.kowalczuk@gmail.com>
2014-04-23 10:39:22 +0100

    order form directive

--- js/directives.js
+++ js/directives.js
@@ -1,10 +1,38 @@
 var sdbDirectives = angular.module('sdbDirectives',[]);
 
 
-sdbDirectives.directive('myTest',[
+sdbDirectives.directive('orderForm',[
     function() {
         return {
-            template: "Is is here?"
+            scope: true,
+            templateUrl: "../templates/orderForm.html",
+            link: function(scope, element, attrs) {
+                scope.order = {};
+                scope.validation = {
+                    name: true,
+                    number: true
+                };
+
+                scope.clickMe = function() {
+                    if(validate()) {
+                        alert('ok ready');
+                    }
+                }
+
+                function validate() {
+                    scope.validation.name = scope.order.name ? true : false;
+                    scope.validation.number = scope.order.number ? true : false;
+
+                    var returnValue = true;
+                    angular.forEach(scope.validation, function(value, index) {
+                        if(!value) {
+                            returnValue = false;
+                        }
+                    });
+                    return returnValue;
+                }
+
+            }
         };
     }
 ]);
